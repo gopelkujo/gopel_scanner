1b2ba9b419debf75d113c5ef833bc91fb7b9d26f
gopelkujo <mn.dwisatya@gmail.com>
2025-07-29 16:07:21 +0700

    feat(home): initialize home page UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,89 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.grey.shade100,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Welcome,',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                          Text(
+                            'User One',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Dummy profile image.
+                    Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    spacing: 16.0,
+                    children: [
+                      Expanded(child: _buildMenuItem(label: 'MRZ')),
+                      Expanded(child: _buildMenuItem(label: 'Fingerprint')),
+                      Expanded(child: _buildMenuItem(label: 'NFC')),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              Text('Gopel Kujo © 2025', style: TextStyle(color: Colors.grey)),
+              const SizedBox(height: 24.0),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildMenuItem({required String label}) {
+    return ElevatedButton(
+      onPressed: () {
+        // TODO: Add on pressed menu item here.
+      },
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black87,
+      ),
+      child: Text(label),
     );
   }
 }
